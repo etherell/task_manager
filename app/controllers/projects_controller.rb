f69ff146e1e164203804a755ac88e908feb41662
etherell <kostyaetherell@gmail.com>
2020-06-27 10:01:39 +0300

    - add CRUD actions in projects controllers - add validations in projects models - add views for projects - add ajax for projects - add toastr notifications

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,53 @@
 class ProjectsController < ApplicationController
+  before_action :find_project!, only: %i[edit update destroy]
+  before_action :flash_clear
+  respond_to :js
+
+  def index
+    @projects = Project.all
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = 'Project was successfully created.'
+    else
+      flash[:error] = @project.errors.full_messages.join("\n")
+      render status: 422
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @project.update(project_params)
+      flash[:success] = 'Project was successfully updated.'
+    else
+      flash[:error] = @project.errors.full_messages.join("\n")
+      render status: 422
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      flash[:success] = 'Project was successfully deleted.'
+    else
+      flash[:error] = @project.errors.full_messages.join("\n")
+      render status: 422
+    end
+  end
+
+  private
+
+  def find_project!
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
+
+  def flash_clear
+    flash.clear
+  end
 end
